dd0a57dfbd3d02e8e6ebeac25648517f1adde22a
KrausAndi <andreas.kraus@substate.de>
2015-09-19 23:40:41 +0200

    first page static html

--- kamelrechnerMU/src/js/scripts.js
+++ kamelrechnerMU/src/js/scripts.js
@@ -1,1 +1,24 @@
-// Empty JS for your own code to be here
+$(document).ready(function(){
+	$('.agerange').on('change', function(){
+
+		var hisage = $(this).val();
+		$('.ageval').text(hisage);
+	})
+	$('.heightrange').on('change', function(){
+		var hisheight = $(this).val();
+		$('.heightval').text(hisheight);
+	})
+	$('input[type=range]').on('input', function () {
+    $(this).trigger('change');
+	});
+
+	$('div.btn-group ul.dropdown-menu li a').click(function (e) {
+    var $div = $(this).parent().parent().parent(); 
+    var $btn = $div.find('.haircolor');
+    $btn.html($(this).text());
+    $div.removeClass('open');
+    e.preventDefault();
+    return false;
+});
+
+})
